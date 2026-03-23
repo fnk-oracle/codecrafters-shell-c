503a092b97ff7954e546df5b8fdea34ba52d48e0
ORACLE <srivastava.raunak13@gmail.com>
2026-03-23 21:04:11 +0530

    Stage 2: Handle invalid commands

--- src/main.c
+++ src/main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+  while (1){
+    printf("$");
 
+    char command[1024];
+    if(fgets(command, 1024, stdin) == NULL){
+      break;
+    }
+
+    command[strcspn(command, "\n")] = '\0';
+
+    if(strcmp(command, "exit 0") == 0){
+      break;
+    }
+    else{
+      printf("%s: command not found\n", command);
+    }
+  }
+  
   return 0;
 }
