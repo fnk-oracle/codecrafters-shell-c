c2aa29000c6457999908903131de705c77040298
ORACLE <srivastava.raunak13@gmail.com>
2026-03-24 15:00:04 +0530

    Echo builtin

--- src/main.c
+++ src/main.c
@@ -20,10 +20,14 @@ int main(int argc, char *argv[]) {
     if(strcmp(command, "exit") == 0){
       break;
     }
+    else if(strncmp(command, "echo ", 5) == 0){
+      printf("%s\n", command + 5);
+    }
     else{
       printf("%s: command not found\n", command);
     }
   }
+  
   
   return 0;
 }
