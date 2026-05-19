4a97e7ff6decd315f3600e74ed90a05bc0d77b17
fnk-oracle <srivastava.raunak13@gmail.com>
2026-05-19 19:58:11 +0530

    Implement single quote parsing for echo and cat tests

--- src/main.c
+++ src/main.c
@@ -1,115 +1,161 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  while (1){
-
+  while (1) {
     printf("$ ");
 
     char command[1024];
-    if(fgets(command, 1024, stdin) == NULL){
+    if (fgets(command, 1024, stdin) == NULL) {
       break;
     }
 
     command[strcspn(command, "\n")] = '\0';
 
-    if(strcmp(command, "exit") == 0){
+    // ---------------------------------------------------------
+    // NEW PARSER: Handles single quotes and preserves spaces
+    // ---------------------------------------------------------
+    char *args[64];
+    int args_count = 0;
+    char *ptr = command;
+
+    // Skip initial leading spaces
+    while (*ptr && isspace((unsigned char)*ptr)) ptr++;
+
+    while (*ptr && args_count < 63) {
+      char buffer[1024] = {0};
+      int buf_idx = 0;
+      bool in_single_quotes = false;
+
+      // Extract a single token
+      while (*ptr) {
+        if (*ptr == '\'') {
+          in_single_quotes = !in_single_quotes; // Toggle state
+          ptr++; // Skip the quote character itself
+        } else if (!in_single_quotes && isspace((unsigned char)*ptr)) {
+          // Break token if we hit a space outside of quotes
+          break;
+        } else {
+          buffer[buf_idx++] = *ptr;
+          ptr++;
+        }
+      }
+
+      // Save token if it contains text or was an empty quoted string ''
+      if (buf_idx > 0 || in_single_quotes) {
+        args[args_count++] = strdup(buffer);
+      }
+
+      // Skip spaces separating the arguments
+      while (*ptr && isspace((unsigned char)*ptr)) ptr++;
+    }
+    args[args_count] = NULL; // Array must be NULL terminated
+
+    // If nothing was entered, continue back to the prompt
+    if (args_count == 0) {
+      continue;
+    }
+    // ---------------------------------------------------------
+
+    // 1. EXIT BUILT-IN
+    if (strcmp(args[0], "exit") == 0) {
+      for (int i = 0; i < args_count; i++) free(args[i]);
       break;
     }
 
-    else if(strncmp(command, "echo ", 5) == 0){
-      printf("%s\n", command + 5);
+    // 2. ECHO BUILT-IN
+    else if (strcmp(args[0], "echo") == 0) {
+      for (int i = 1; i < args_count; i++) {
+        printf("%s", args[i]);
+        if (i < args_count - 1) {
+          printf(" ");
+        }
+      }
+      printf("\n");
     }
 
-    else if(strncmp(command, "type ", 5) == 0){
-      char *arg = command +5;
+    // 3. TYPE BUILT-IN
+    else if (strcmp(args[0], "type") == 0) {
+      if (args_count < 2) {
+        printf("type: missing operand\n");
+      } else {
+        char *arg = args[1];
 
-      //1. Checking Builtins first
-      if(strcmp(arg, "echo") == 0 || strcmp(arg, "exit") == 0 || strcmp(arg, "type") == 0 || strcmp(arg, "pwd") == 0){
-        printf("%s is a shell builtin\n", arg);
-      } 
-      else{
-        //2. Check PATH for executables
-        char *path_env = getenv("PATH");
-        char *path_copy = strdup(path_env);
-        char *dir = strtok(path_copy, ":");
-        int found = 0;
+        // Checking Builtins first
+        if (strcmp(arg, "echo") == 0 || strcmp(arg, "exit") == 0 || strcmp(arg, "type") == 0 || strcmp(arg, "pwd") == 0) {
+          printf("%s is a shell builtin\n", arg);
+        } 
+        else {
+          // Check PATH for executables
+          char *path_env = getenv("PATH");
+          int found = 0;
 
-        while(dir != NULL){
-          char full_path[1024];
-          snprintf(full_path, sizeof(full_path), "%s/%s", dir, arg);
+          if (path_env != NULL) {
+            char *path_copy = strdup(path_env);
+            char *dir = strtok(path_copy, ":");
 
-          if (access(full_path, X_OK) == 0){
-            printf("%s is %s\n", arg, full_path);
-            found = 1;
-            break;
+            while (dir != NULL) {
+              char full_path[1024];
+              snprintf(full_path, sizeof(full_path), "%s/%s", dir, arg);
+
+              if (access(full_path, X_OK) == 0) {
+                printf("%s is %s\n", arg, full_path);
+                found = 1;
+                break;
+              }
+              dir = strtok(NULL, ":");
+            }
+            free(path_copy);
           }
-          dir = strtok(NULL, ":");
-        }
 
-        if(!found){
-          printf("%s: not found\n", arg);
+          if (!found) {
+            printf("%s: not found\n", arg);
+          }
         }
-
-       
       }
     }
 
-    else if(strcmp(command, "pwd") == 0){
+    // 4. PWD BUILT-IN
+    else if (strcmp(args[0], "pwd") == 0) {
       char cwd[1024];
-      if(getcwd(cwd, sizeof(cwd)) != NULL){
+      if (getcwd(cwd, sizeof(cwd)) != NULL) {
         printf("%s\n", cwd);
-      }
-      else{
+      } else {
         perror("getcwd() error");
       }
     }
 
-
-    else{
-      // 1. Prepare arguments for execvp
-      char *args[64];
-      int i = 0;
-      char *token = strtok(command, " ");
-
-      while(token != NULL){
-        args[i++] = token;
-        token = strtok(NULL, " ");
-      }
-
-      args[i] = NULL; //Must be NULL terminated
-
-      //2. Try to runt the program
+    // 5. EXTERNAL COMMAND EXECUTION
+    else {
       pid_t pid = fork();
-      if(pid == 0){
-        //In the CHILD process
-          if(execvp(args[0], args) == -1){
-            //If it fails to find/run the program, THEN print the error
-            printf("%s: command not found\n", args[0]);
-            exit(1);
-          }
-
-      }
-      else if (pid > 0){
-        //In the PARENT process
+      if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+          printf("%s: command not found\n", args[0]);
+          exit(1);
+        }
+      } 
+      else if (pid > 0) {
         wait(NULL);
-      }
-      else{
+      } 
+      else {
         perror("fork");
       }
     }
 
-    
+    // Clean up memory allocated by strdup
+    for (int i = 0; i < args_count; i++) {
+      free(args[i]);
+    }
   }
-  
-  
+
   return 0;
 }
